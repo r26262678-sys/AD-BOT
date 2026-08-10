5ee542695ac3324e444fc45ad1f854c7eb26529c
r26262678-sys <r26262678@gmail.com>
2026-08-10 15:41:47 +0330

    Update adbot.py

--- adbot.py
+++ adbot.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 # تنظیمات پیش‌فرض
 CONFIG_FILE = "bot_config.json"
 DEFAULT_CONFIG = {
-    "host_usernames": ["ad0ri"],
+    "host_usernames": ["king_alman"],
     "admin_usernames": ["ad0ri"],
     "vip_usernames": [],
     "banned_users": [],
@@ -2713,8 +2713,8 @@ async def main():
     from http.server import BaseHTTPRequestHandler, HTTPServer
     
     logger.info("تلاش برای بارگذاری متغیرهای محیطی...")
-    room_id = os.getenv("ROOM_ID", "68e771922d585712212e8070")
-    api_token = os.getenv("API_TOKEN", "ed578ca3bb6a6592059a878e4d807d0c7eac56742239fdcce7ca84841f2437e5")
+    room_id = os.getenv("ROOM_ID", "https://create.highrise.game/asset-catalog/6a78855c30ceb0c593a231fd")
+    api_token = os.getenv("API_TOKEN", "7725315a6f1c7e9c8923693bc0d23ad89ab65b3370866402cb34df681150845d")
     
     if not room_id or not api_token:
         logger.error("ROOM_ID یا API_TOKEN تنظیم نشده‌اند.")
